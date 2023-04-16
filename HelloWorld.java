3a273025ffd57e5631b1d9ac7999e1403074f145
tarunakhare <58461159+tarunakhare@users.noreply.github.com>
2023-04-16 11:16:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Tarun!"); 
+         System.out.println("Hello Tarun!, welcome to devops training"); 
     }
 }
